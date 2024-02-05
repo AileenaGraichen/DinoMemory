e7d152d92c622c2eb9c7dc03075592a09e250eba
Aileena <aile0074@stud.kea.dk>
2024-02-05 11:12:50 +0100

    setup game logic

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ window.addEventListener("load", start);
 
 const images =["1.png","2.png","3.png","4.png","5.png","6.png","7.png","8.png","9.png","10.png"];
 const CARDS_ON_BOARD = 20;
+let selectedCard = null;
 
 function start() {
-    console.log("JavaScript is running!");
+    initializeCards();
+    makeCardsClickable();
 }
 
 function initializeCards(){
@@ -31,10 +33,42 @@ function initializeCards(){
 
 function displayCards(cards){
     const elements = document.querySelectorAll(".card");
-    console.log(elements);
+  
     elements.forEach((element, index) => {
+        element.dataset.image = cards[index];
         const image = element.querySelector("img");
         image.src = "Images/" + images[cards[index]];
     });
+}
+
+function makeCardsClickable(){
+    document.querySelectorAll(".card").forEach(card =>
+        card.addEventListener("click", (event) => clickCard(card)));
+}
+
+function clickCard(card){
+    if(selectedCard){
+        selectedCard.classList.add("selected");
+        const firstCard = selectedCard;
+        const secondCard = card;
+
+        console.log("Two cards selected");
+        console.log(firstCard);
+        console.log(secondCard);
+
+        if(firstCard.dataset.image == secondCard.dataset.image){
+            console.log("It's a match!");
+        } else {
+            firstCard.classList.remove("selected");
+            secondCard.classList.remove("selected");
+            console.log("It's not a match!");
+        }
+
+        selectedCard = null;
+
+    } else {
+        card.classList.add("selected");
+        selectedCard = card;
+    }
 }
 
